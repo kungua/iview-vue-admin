f18f2eb476dae367683523df15072530b4c66111
kungua <maguahongkun@outlook.com>
2019-12-31 14:06:45 +0800

    vuex mutations

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,28 +1,30 @@
 <template>
   <div>
-    <!--    <p>appNameWithVersion: {{ appNameWithVersion }}</p>-->
-    <p>userNameFirstLetter: {{ userNameFirstLetter }}</p>
-    <a-input @input="handleInput"/>
-    <p>Text: {{ inputValue }}</p>
-    <a-show :content="inputValue"/>
     <p>appName: {{ appName }}</p>
-    <p>userName: {{ userName }}</p>
-    <p>{{ inputValue }} -> lastLetter is : {{ inputValueLastLetter }}</p>
+    <button @click="handleChangeAppName">点我改变 appName</button>
+    <!--    <p>appNameWithVersion: {{ appNameWithVersion }}</p>-->
+    <!--    <p>userNameFirstLetter: {{ userNameFirstLetter }}</p>-->
+    <!--    <a-input @input="handleInput"/>-->
+    <!--    <p>Text: {{ inputValue }}</p>-->
+    <!--    <a-show :content="inputValue"/>-->
+    <!--    <p>userName: {{ userName }}</p>-->
+    <!--    <p>{{ inputValue }} -> lastLetter is : {{ inputValueLastLetter }}</p>-->
   </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
 // import { createNamespacedHelpers } from 'vuex'
-import { mapState, mapGetters } from 'vuex'
-import AInput from '../components/AInput'
-import AShow from '../components/AShow'
+// import { mapState, mapGetters } from 'vuex'
+// import AInput from '../components/AInput'
+// import AShow from '../components/AShow'
 
 // const { mapState } = createNamespacedHelpers('user')
 export default {
   name: 'Store',
   components: {
-    AInput,
-    AShow
+    // AInput,
+    // AShow
   },
   data () {
     return {
@@ -33,10 +35,10 @@ export default {
     // appNameWithVersion () {
     //   return this.$store.getters.appNameWithVersion
     // },
-    ...mapGetters('user', ['userNameFirstLetter']),
-    inputValueLastLetter () {
-      return this.inputValue.substr(-1, 1)
-    },
+    // ...mapGetters('user', ['userNameFirstLetter']),
+    // inputValueLastLetter () {
+    //   return this.inputValue.substr(-1, 1)
+    // },
     // // 方法五
     // ...mapState('user', {
     //   userName: state => state.userName
@@ -46,24 +48,29 @@ export default {
     //   userName: state => state.userName
     // })
     // 方法三
-    ...mapState({
-      appName: state => state.appName,
-      userName: state => state.user.userName
-    })
+    // ...mapState({
+    //   appName: state => state.appName,
+    //   userName: state => state.user.userName
+    // })
     // 方法二
     // ...mapState(['appName', 'userName'])
     // 方法一
-    // appName () {
-    //   return this.$store.state.appName
-    // },
+    appName () {
+      return this.$store.state.appName
+    }
     // userName () {
     //   return this.$store.state.user.userName
     // }
   },
   methods: {
-    handleInput (val) {
-      this.inputValue = val
+    ...mapMutations(['SET_APP_NAME']),
+    handleChangeAppName () {
+      // this.$store.commit('SET_APP_NAME', 'magua')
+      this.SET_APP_NAME('magua2')
     }
+    // handleInput (val) {
+    //   this.inputValue = val
+    // }
   }
 }
 </script>
