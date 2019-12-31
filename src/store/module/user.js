b7f54589e7681ad87fa8413e3cd4ca6bd526d517
kungua <maguahongkun@outlook.com>
2019-12-31 12:31:36 +0800

    vuex state

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -1,5 +1,5 @@
 const state = {
-  //
+  userName: '剑来'
 }
 
 const mutations = {
@@ -11,6 +11,7 @@ const actions = {
 }
 
 export default {
+  namespaced: true,
   state,
   mutations,
   actions,
--- src/views/Store.vue
+++ src/views/Store.vue
@@ -3,13 +3,18 @@
     <a-input @input="handleInput"/>
     <p>Text: {{ inputValue }}</p>
     <a-show :content="inputValue"/>
+    <p>appName: {{ appName }}</p>
+    <p>userName: {{ userName }}</p>
   </div>
 </template>
 
 <script>
+// import { createNamespacedHelpers } from 'vuex'
+import { mapState } from 'vuex'
 import AInput from '../components/AInput'
 import AShow from '../components/AShow'
 
+// const { mapState } = createNamespacedHelpers('user')
 export default {
   name: 'Store',
   components: {
@@ -20,6 +25,30 @@ export default {
     return {
       inputValue: ''
     }
+  },
+  computed: {
+    // // 方法五
+    // ...mapState('user', {
+    //   userName: state => state.userName
+    // })
+    // 方法四
+    // ...mapState({
+    //   userName: state => state.userName
+    // })
+    // 方法三
+    ...mapState({
+      appName: state => state.appName,
+      userName: state => state.user.userName
+    })
+    // 方法二
+    // ...mapState(['appName', 'userName'])
+    // 方法一
+    // appName () {
+    //   return this.$store.state.appName
+    // },
+    // userName () {
+    //   return this.$store.state.user.userName
+    // }
   },
   methods: {
     handleInput (val) {
