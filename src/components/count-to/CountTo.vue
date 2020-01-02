681e8d0f4ef5548ab62cfc9e62f1e99b876aa26d
kungua <maguahongkun@outlook.com>
2020-01-02 10:13:47 +0800

    完成了 countTo 组件

--- src/components/count-to/CountTo.vue
+++ src/components/count-to/CountTo.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    count-to
-    <span :id="eleId"></span>
+    <slot name="left"></slot>
+    <span :id="eleId" :class="countClass" ref="number"></span>
+    <slot name="right"></slot>
   </div>
 </template>
 
 <script>
 import CountUp from 'countup'
+// import './CountTo.less'
 
 export default {
   name: 'CountTo',
@@ -14,23 +16,102 @@ export default {
     /**
      * @description 起始值
      */
-    startVal: {
-      type: Number,
-      default: 0
-    },
+    startVal: { type: Number, default: 0 },
+    /**
+     * @description 最终值
+     */
+    endVal: { type: Number, required: true },
+    /**
+     * @description 小数点后保留几位小数
+     */
+    decimals: { type: Number, default: 0 },
+    /**
+     * @description 渐变时长
+     */
+    duration: { type: Number, default: 1 },
+    /**
+     * @description 是否使用变速效果
+     */
+    useEasing: { type: Boolean, default: false },
+    /**
+     * @description 是否使用分组效果
+     */
+    useGrouping: { type: Boolean, default: true },
+    /**
+     * @description 分组符号
+     */
+    separator: { type: String, default: ',' },
+    /**
+     * @description 整数和小数分割符号
+     */
+    decimal: { type: String, default: '.' },
+    /**
+     * @description 动画延迟开始时间
+     */
+    delay: { type: Number, default: 0 },
+    /**
+     * @description 外部传进来的类名
+     */
+    className: { type: String, default: '' }
+  },
+  data () {
+    return {
+      counter: {}
+    }
   },
   computed: {
     eleId () {
       return `count_up_${this._uid}`
+    },
+    countClass () {
+      return ['count-to-number', this.className]
+    }
+  },
+  watch: {
+    endVal (newVal, oldVal) {
+      this.counter.update(newVal)
+      this.emitEndEvent()
     }
   },
   mounted () {
+    const {
+      eleId,
+      startVal,
+      endVal,
+      decimals,
+      duration,
+      useEasing,
+      useGrouping,
+      separator,
+      decimal,
+      delay
+    } = this
     this.$nextTick(() => {
-      const { startVal } = this
-      const counter = new CountUp('count_up', startVal)
+      this.counter = new CountUp(eleId, startVal, endVal, decimals, duration, {
+        useEasing,
+        useGrouping,
+        separator,
+        decimal
+      })
     })
+    setTimeout(() => {
+      this.counter.start()
+      this.emitEndEvent()
+    }, delay)
+  },
+  methods: {
+    getCount () {
+      return this.$refs.number.innerText
+    },
+    emitEndEvent () {
+      setTimeout(() => {
+        this.$emit('on-animation-end', Number(this.getCount()))
+      }, this.duration * 1000 + 23)
+    }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+@import "./CountTo.less";
+</style>
--- src/views/CountTo.vue
+++ src/views/CountTo.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>count to
-    <count-to/>
-    <count-to/>
-    <count-to/>
+  <div>
+    <div>这是一个使用 CountTo 组件的页面</div>
+    <count-to class-name="klass3" :end-val="30" :duration="0.7"/>
+    <button @click="getNumber">获取数值</button>
+    <button @click="up">更新数值</button>
+    <count-to
+      :end-val="endVal"
+      @on-animation-end="handleEnd"
+      class-name="klass1"
+      ref="countTo"
+    >
+      <span slot="left">总金额: </span>
+      <span slot="right">元</span>
+    </count-to>
+    <count-to class-name="klass2" :end-val="30000" :duration="2.8"/>
   </div>
 </template>
 
@@ -13,6 +24,22 @@ export default {
   name: 'CountToPage',
   components: {
     CountTo
+  },
+  data () {
+    return {
+      endVal: 40
+    }
+  },
+  methods: {
+    handleEnd (endVal) {
+      console.log('endVal -> ', endVal)
+    },
+    up () {
+      this.endVal = this.endVal + Math.random() * 100 + 1
+    },
+    getNumber () {
+      console.log(this.$refs.countTo.getCount())
+    }
   }
 }
 </script>
